cda1ecf0682d71d747111b29ce00b186a6cd7f3d
Андрей Хохлов <andrey@MacBook-Air-Andrej.local>
2024-06-15 15:58:47 +0300

    Modified app.js, menu.js and add file test.module.js

--- src/app.js
+++ src/app.js
@@ -1,2 +1,19 @@
-import './styles.css'
+import './styles.css';
+import { ContextMenu } from './menu.js';
+import { TestModule } from './modules/test.module.js'; // Подключаем свой модуль вместо этого
 
+const menu = new ContextMenu('#menu');
+const test = new TestModule('test', 'Тест'); // создаем свой экземпляр класса. Первый параметр оставляем test. Имя переменной оставляем test.
+
+menu.open();
+menu.add(test);
+
+document.body.addEventListener('click', handleClick);
+
+function handleClick(event) {
+  const { target } = event;
+
+  if (target.dataset.type === 'test') {
+    test.trigger();
+  }
+}
