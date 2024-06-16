e58cdf9142576ffff34cf8893a957ecb350121c5
Андрей Хохлов <andrey@MacBook-Air-Andrej.local>
2024-06-16 17:30:38 +0300

    Add file clicks.module.js and modified app.js

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 import './styles.css';
 import { ContextMenu } from './menu.js';
+import { ClicksModule } from './modules/clicks.module.js';
 import { ShapeModule } from './modules/shape.module.js';
 import { SoundModule } from './modules/sound.module.js';
 import { TestModule } from './modules/test.module.js'; // Подключаем свой модуль вместо этого
 
 const modules = [
+  new ClicksModule('clicks', 'Считать клики (за 3 секунды)'),
   new ShapeModule('shape', 'Случайная фигура'),
   new SoundModule('sound', 'Случайный звук'),
   new TestModule('test', 'Тест'),
