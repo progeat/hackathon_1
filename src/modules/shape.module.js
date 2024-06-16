178e1c3af725ed3932e833f4961dd22019f893bc
Stakrotsky <Stakrotsky@gmail.com>
2024-06-16 21:39:07 +0300

    fix bug

--- src/modules/shape.module.js
+++ src/modules/shape.module.js
@@ -4,58 +4,61 @@ import { random, randomColor } from '../utils';
 export class ShapeModule extends Module {
   constructor(type, text) {
     super(type, text);
+    this.canvas = null;
+    this.canvasCTX = null;
   }
 
   trigger() {
-    const addCanvasHTML = document.createElement('canvas');
-    const canvasCTX = addCanvasHTML.getContext('2d');
-    addCanvasHTML.width = window.innerWidth - 100;
-    addCanvasHTML.height = window.innerHeight - 100;
-    document.body.appendChild(addCanvasHTML);
-
-    function createRandomShape() {
+    if (!this.canvas) {
+      this.canvas = document.createElement('canvas');
+      this.canvasCTX = this.canvas.getContext('2d');
+      this.canvas.width = window.innerWidth - 100;
+      this.canvas.height = window.innerHeight - 100;
+      document.body.appendChild(this.canvas);
+    }
+    const createRandomShape = () => {
       const shapeType = random(0, 4);
-      const x = random(0, addCanvasHTML.width);
-      const y = random(0, addCanvasHTML.height);
+      const x = random(0, this.canvas.width);
+      const y = random(0, this.canvas.height);
       const size = random(20, 100);
       const color = randomColor();
 
-      canvasCTX.fillStyle = color;
+      this.canvasCTX.fillStyle = color;
 
       switch (shapeType) {
         case 0:
-          canvasCTX.fillRect(x, y, size, size);
+          this.canvasCTX.fillRect(x, y, size, size);
           break;
         case 1:
           // Circle
-          canvasCTX.beginPath();
-          canvasCTX.arc(x, y, size / 2, 0, Math.PI * 2);
-          canvasCTX.fill();
+          this.canvasCTX.beginPath();
+          this.canvasCTX.arc(x, y, size / 2, 0, Math.PI * 2);
+          this.canvasCTX.fill();
           break;
         case 2:
-          canvasCTX.beginPath();
-          canvasCTX.moveTo(x, y);
-          canvasCTX.lineTo(x + size, y);
-          canvasCTX.lineTo(x + size / 2, y - size);
-          canvasCTX.closePath();
-          canvasCTX.fill();
+          this.canvasCTX.beginPath();
+          this.canvasCTX.moveTo(x, y);
+          this.canvasCTX.lineTo(x + size, y);
+          this.canvasCTX.lineTo(x + size / 2, y - size);
+          this.canvasCTX.closePath();
+          this.canvasCTX.fill();
           break;
         case 3:
-          canvasCTX.beginPath();
-          canvasCTX.ellipse(x, y, size, size / 2, 0, 0, Math.PI * 2);
-          canvasCTX.fill();
+          this.canvasCTX.beginPath();
+          this.canvasCTX.ellipse(x, y, size, size / 2, 0, 0, Math.PI * 2);
+          this.canvasCTX.fill();
           break;
         case 4:
-          canvasCTX.beginPath();
-          canvasCTX.moveTo(x, y);
-          canvasCTX.lineTo(x + size / 2, y + size / 2);
-          canvasCTX.lineTo(x, y + size);
-          canvasCTX.lineTo(x - size / 2, y + size / 2);
-          canvasCTX.closePath();
-          canvasCTX.fill();
+          this.canvasCTX.beginPath();
+          this.canvasCTX.moveTo(x, y);
+          this.canvasCTX.lineTo(x + size / 2, y + size / 2);
+          this.canvasCTX.lineTo(x, y + size);
+          this.canvasCTX.lineTo(x - size / 2, y + size / 2);
+          this.canvasCTX.closePath();
+          this.canvasCTX.fill();
           break;
       }
-    }
+    };
     setInterval(createRandomShape, 1000);
   }
   toHTML() {
